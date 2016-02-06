d7cf0b18258b0f02e170d835dc6d9500c6d03ae1
Jonathan Freeman <freethejazz@gmail.com>
2016-02-06 13:37:32 -0600

    Persists simple consumer to mongodb

--- simple-consumer/app.js
+++ simple-consumer/app.js
@@ -2,35 +2,67 @@
 
 let nconf = require('nconf');
 let amqplib = require('amqplib');
-let mongo = require('mongodb')
+let MongoClient = require('mongodb').MongoClient;
 
 // Do the config thing
 nconf.argv()
   .env()
   .file({file: '../config.json'});
 
-let ex = nconf.get('rabbit_exchange');
-let qName = nconf.get('simple_queue');
+const ex = nconf.get('rabbit_exchange');
+const qName = nconf.get('simple_queue');
+const mongoUrl = `mongodb://${nconf.get('mongo_host')}:27017/superTweets`;
+
+let writeDoc = function(coll, doc) {
+  coll.insert(doc, function(err, result) {
+    if(err) {
+      console.error(err);
+    } else {
+      console.log(`Wrote doc with id: ${doc.id}`);
+    }
+  });
+};
 
 // Connect to mongo
+var onMongo = new Promise((resolve, reject) => {
+  MongoClient.connect(mongoUrl, (err, db) => {
+    if(err) {
+      reject(err);
+    }
+
+    else resolve(db.collection('simple'));
+  });
+});
+
+onMongo.catch((err) => {
+  console.error(err);
+});
 
 // Connect to Rabbit and start consuming
-let open = amqplib.connect(`amqp://${nconf.get('rabbit_host')}`);
-open.then((conn) => {
-  conn.createChannel()
+onMongo.then((coll) => {
+  let open = amqplib.connect(`amqp://${nconf.get('rabbit_host')}`);
+  open.then((conn) => {
+    conn.createChannel()
     .then((ch) => {
       console.log('Created Channel, asserting exchange');
       ch.assertExchange(ex, 'fanout', {durable: false});
       console.log('Created exchange, asserting queue');
 
       ch.assertQueue(qName, {})
-        .then((q) => {
+      .then((q) => {
         console.log(` [*] Waiting for messages in ${q.queue}. To exit press CTRL+C`);
         ch.bindQueue(q.queue, ex, '');
-
         ch.consume(q.queue, (msg) => {
-          console.log(" [x] %s", msg.content.toString());
+          let doc;
+          try {
+            doc = JSON.parse(msg.content.toString());
+          } catch (e) {
+            return console.warn('Could not parse message into JSON');
+          }
+
+          writeDoc(coll, doc);
         }, {noAck: true});
       });
     });
+  });
 });
