6c054488acd487b9fc2c9e3322b3fac269355c30
Jonathan Freeman <freethejazz@gmail.com>
2016-02-06 19:16:25 -0600

    Adds initial API w/ counts and counts by team

---
+++ api/app.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const db = require('./db');
+const Hapi = require('hapi');
+
+// Create a server with a host and port
+const server = new Hapi.Server();
+
+server.connection({
+  host: 'localhost',
+  port: 8000
+});
+
+// Count
+server.route({
+  method: 'GET',
+  path:'/count',
+  handler: function (request, reply) {
+    db.count().then((num) => {
+      reply({
+        status: 200,
+        data: {
+          count: num
+        }
+      });
+    });
+  }
+});
+
+// Team Counts
+server.route({
+  method: 'GET',
+  path:'/countByTeam',
+  handler: function (request, reply) {
+    db.teamCounts().then((doc) => {
+      reply({
+        status: 200,
+        data: doc
+      });
+    });
+  }
+});
+
+// Start the server
+server.start((err) => {
+
+  if (err) {
+    throw err;
+  }
+  console.log('Server running at:', server.info.uri);
+});
---
+++ api/db.js
@@ -0,0 +1,66 @@
+'use strict';
+
+let MongoClient = require('mongodb').MongoClient;
+let nconf = require('nconf');
+
+// Do the config thing
+nconf.argv()
+  .env()
+  .file({file: '../config.json'});
+
+const mongoUrl = `mongodb://${nconf.get('mongo_host')}:27017/superTweets`;
+
+// Connect to mongo
+var onMongo = new Promise((resolve, reject) => {
+  MongoClient.connect(mongoUrl, (err, db) => {
+    if(err) {
+      reject(err);
+    }
+
+    else resolve(db.collection('sentiment'));
+  });
+});
+
+onMongo.catch((err) => {
+  console.error(err);
+});
+
+let q = function(fn) {
+  return new Promise((resolve, reject) => {
+    onMongo.then((coll) => {
+      fn(coll).then(resolve).catch(reject);
+    });
+  });
+};
+
+let agg = function(pipeline) {
+  return q((coll) => {
+    return new Promise((resolve, reject) => {
+      coll.aggregate(pipeline, (err, result) => {
+        if(err) {
+          reject(err);
+        } else {
+          resolve(result)
+        }
+      });
+    })
+  })
+};
+
+let count = function() {
+  return q((coll) => coll.count());
+};
+
+let teamCounts = function() {
+  return agg([
+    { $group: {
+      _id: "$affiliation",
+      count: { $sum: 1 }
+    }}
+  ]);
+};
+
+module.exports = {
+  count,
+  teamCounts
+}
